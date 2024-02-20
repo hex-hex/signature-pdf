1b91f664280f6d92e6bf18a23d65deabfeff35b3
Hex <he.x.working@gmail.com>
2024-02-21 11:02:34 +1300

    app

--- signature_pdf/settings.py
+++ signature_pdf/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "signature_pdf.signature.apps.SignatureConfig"
 ]
 
 MIDDLEWARE = [
